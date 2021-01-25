d0c91a7f83babfad135dc4b33658110c789bb958
Alyssa Biot <alyssa.biot@gmail.com>
2021-01-25 17:44:53 +0100

    add basic auth to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  name = ENV['APP_USERNAME']
+  password = ENV['APP_PASSWORD']
+  http_basic_authenticate_with name: name, password: password
 end
